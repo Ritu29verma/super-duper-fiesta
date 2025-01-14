15bb4e229a62fe4614594187713b65be95dad1f3
Ritu29verma <ritu1298verma@gmail.com>
2025-01-14 22:28:54 +0530

    #

--- BE/index.js
+++ BE/index.js
@@ -1,22 +1,32 @@
-const express = require ('express')
-const mongoose = require ('mongoose')
-const cors = require ('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 const TodoRoutes = require("./routes/todos");
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 const bodyParser = require('body-parser');
-app.use(cors())
-app.use(express.json())
+const path = require("path");
+
+app.use(cors({ "origin": "*" }));
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Use the built-in __dirname
+const distPath = path.join(__dirname, "../FE/dist");
+app.use(express.static(distPath));
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.use("/api/v1", TodoRoutes);
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(distPath, "index.html"));
+});
 
-app.listen( port , () => {
-    console.log('Server is running on port 3000')
-})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
